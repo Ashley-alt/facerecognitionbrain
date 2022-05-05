7b7868cc578ea0e00325d270b036a2c46d7dbb48
ashley-alt <a.olubanjo1@gmail.com>
2022-05-05 16:58:15 +0100

    adding logo and sign out components

---
+++ src/components/logo/logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './logo.css';
+
+const logo = () => {
+    return (
+        <div className="ma4 mt0" id="brain">
+            <img src="https://i.pinimg.com/originals/6a/8f/8f/6a8f8f9f5f34560405de991efefd6e82.png" alt="brain logo" width ="300" height ="200" />
+        </div>
+    )
+}
+
+export default logo;
