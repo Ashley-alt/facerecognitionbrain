4230b54bc1e4abf49339d64fe5c25efc9494ece2
ashley-alt <a.olubanjo1@gmail.com>
2022-05-05 16:57:29 +0100

    added logo and sign out components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { render } from '@testing-library/react';
 import React, {Component} from 'react';
+import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
 import './App.css';
+import 'tachyons'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Navigation></Navigation>
+        <Logo></Logo>
+        {/*<ImageLinkForm></ImageLinkForm>
+    <FaceRecognition></FaceRecognition>*/}
       </div>
     );
   }
